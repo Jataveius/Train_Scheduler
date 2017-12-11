34329d58b9263f485cae6cf6c7dcfa0cb6119c70
Ashley Louitan <jataveius@aol.com>
2017-12-10 21:50:39 -0500

    add files

--- js/train.js
+++ js/train.js
@@ -1,9 +1,9 @@
-$(document).ready(fuction(){
-    var name: ;
-    var train: $("#name-input").val().trim();
-    var line: $("#lineInput").val().trim();
-    var stop:
-    var timeForTrain:
-    var freq:
+// $(document).ready(fuction(){
+//     var name: ;
+//     var train: $("#name-input").val().trim();
+//     var line: $("#lineInput").val().trim();
+//     var stop:;
+//     var timeForTrain:;
+//     var freq:;
 
-}
+// }
