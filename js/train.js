5e14e0a913c5a06ac279ce34b7b5ce725cc3c445
Ashley Louitan <jataveius@aol.com>
2017-12-12 00:08:57 -0500

    add files

--- js/train.js
+++ js/train.js
@@ -1,13 +1,4 @@
-// $(document).ready(fuction(){
-
-//     $("#trainbtnadd").on("click",function(){
-//     var train: $("#train-name-input").val().trim();
-//     var line: $("#lineInput").val().trim();
-//     var destination: $("#destination-input").val().trim();
-//     var timeForTrain:;
-//     var frequency:;
-
-// }
+$( document ).ready(function() {
 
 
       // Initialize Firebase
@@ -19,4 +10,82 @@
         storageBucket: "train-schedule-60315.appspot.com",
         messagingSenderId: "191259008691"
       };
-    //   firebase.initializeApp(config);
+      firebase.initializeApp(config);
+      var database = firebase.database();
+        
+        
+            $("#trainbtnadd").on("click",function(event){
+            event.preventDefault();
+            
+                var train= $("#train-name-input").val().trim();
+            var destination =$("#destination-input").val().trim();
+            var firstTrainInput= moment($("#first-train-input").val().trim(), "hh:mm").subtract(1, "years").format("X");
+            var frequency= $("#frequency-input").val().trim();
+        
+
+		// Console.log
+		console.log(train);
+		console.log(destination);
+		console.log(firstTrainInput);
+        console.log(frequency);
+        
+        var currentTime = moment();
+        console.log("CURRENT TIME: " +  moment(currentTime).format("hh:mm"));
+
+        // 	// clears ID
+		// $("#train-name-input").val("");
+		// $("#destination-input").val("");
+		// $("#first-train-input").val("");
+		// $("#frequency-input").val("");
+        
+
+    var newTrainArriving ={
+        trains:train,
+        departing:destination,
+        arriving:firstTrainInput,
+        onMin: frequency,
+        minutes:minutes,
+	
+};
+
+
+ // Push to database
+ database.ref().push(newTrainArriving);
+
+ 	// Clears ID
+     $("#train-name-input").val("");
+     $("#destination-input").val("");
+     $("#first-train-input").val("");
+     $("#frequency-input").val("");
+
+});
+     database.ref().on("child_added", function(childSnapshot, prevChildKey){
+		console.log(childSnapshot.val());
+
+		// Snapshot for Firebase.
+		var firebaseTrain = childSnapshot.val().trains;
+		var firebaseDestination = childSnapshot.val().departing;
+		var firebaseArriving = childSnapshot.val().arriving;
+		var firebaseFrequency = childSnapshot.val().onMin;
+		
+		var diffTime = moment().diff(moment.unix(firebaseArriving), "minutes");
+		var remain = moment().diff(moment.unix(firebaseArriving), "minutes") % firebaseFrequency ;
+		var minutes = firebaseFrequency - remain;
+
+		var nextTrainArrival = moment().add(minutes, "m").format("hh:mm A"); 
+		
+	// Console.log
+		console.log(minutes);
+		console.log(nextTrainArrival);
+		console.log(moment().format("hh:mm A"));
+		console.log(nextTrainArrival);
+		console.log(moment().format("X"));
+
+		// Append 
+        $("#trainTable > tbody").append("<tr><td>" + firebaseTrain + "</td><td>" + 
+        firebaseDestination + "</td><td>" + firebaseFrequency + " mins" + "</td><td>" + 
+        nextTrainArrival + "</td><td>" + minutes + "</td></tr>");
+
+    });
+});
+
